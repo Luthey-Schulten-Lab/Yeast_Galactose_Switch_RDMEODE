143734381bb7bddab0f5b7841fffe0e341b15511
foxhunter <shield75@outlook.com>
2026-02-07 20:16:37 -0600

    chrmo color change to purple:

--- video_rendering_VMD/high_res_video.tcl
+++ video_rendering_VMD/high_res_video.tcl
@@ -35,4 +35,4 @@ proc make_movie_png {} {
     }
 }
 
-#ffmpeg -framerate 24 -pattern_type glob -i '*.png'   -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2"   -c:v libx264 -pix_fmt yuv420p -crf 18 demo_movie_ER.mp4
+#ffmpeg -framerate 24 -pattern_type glob -i '*.png'   -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2"   -c:v libx264 -pix_fmt yuv420p -crf 18 S5_eff.mp4
