805692ed4aa31fdd17bb816c0f7993419ff819df
foxhunter <shield75@outlook.com>
2026-02-11 15:26:32 -0600

    update video script

--- video_rendering_VMD/high_res_video.tcl
+++ video_rendering_VMD/high_res_video.tcl
@@ -34,5 +34,7 @@ proc make_movie_png {} {
         render TachyonLOptiXInternal $filename
     }
 }
-
+#ffmpeg -framerate 24 -pattern_type glob -i '*.png'   -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2"   -c:v libx264 -pix_fmt yuv420p -crf 18 S2_baseline.mp4
+#ffmpeg -framerate 24 -pattern_type glob -i '*.png'   -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2"   -c:v libx264 -pix_fmt yuv420p -crf 18 S3_chromosome.mp4
+#ffmpeg -framerate 24 -pattern_type glob -i '*.png'   -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2"   -c:v libx264 -pix_fmt yuv420p -crf 18 S4_ER.mp4
 #ffmpeg -framerate 24 -pattern_type glob -i '*.png'   -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2"   -c:v libx264 -pix_fmt yuv420p -crf 18 S5_eff.mp4
